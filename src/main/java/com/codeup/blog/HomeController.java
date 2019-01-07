bce6cba25a7852dd1eecae80f94443d9cc85e910
Enrique Hernandez <hernandez.x.enrique@gmail.com>
2019-01-07 14:19:34 -0600

    added HelloController.java, HomeController.java, controllers/HomeController.java and completed HomeController exercise

---
+++ src/main/java/com/codeup/blog/HomeController.java
@@ -0,0 +1,16 @@
+//package com.codeup.blog;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//
+//@Controller
+//public class HomeController {
+//
+//	@GetMapping ("/") // the piece of URL we're listening for
+//	@ResponseBody // only necessary if your're returning a string/number
+//	public String home () {
+//		return "Welcome Home!";
+//	}
+//
+//}
