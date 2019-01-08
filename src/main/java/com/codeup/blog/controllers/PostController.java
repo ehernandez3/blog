4c6a8812ccd1bba4092022a5a87f052f75de00d0
Enrique Hernandez <hernandez.x.enrique@gmail.com>
2019-01-08 15:32:51 -0600

    completed all exercises

--- src/main/java/com/codeup/blog/controllers/PostController.java
+++ src/main/java/com/codeup/blog/controllers/PostController.java
@@ -1,21 +1,53 @@
 package com.codeup.blog.controllers;
 
+import com.codeup.blog.models.Post;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
 public class PostController {
 
-	@GetMapping ("/posts")
-	public String index() {
-		return "Will display all posts";
+	// Edit your PostController class to return the views you created above.
+	//
+	//Inside the method that shows an individual post, create a new post object
+	// and pass it to the view.
+
+	//Inside the method that shows all the posts, create a new array list and add
+	// two post objects to it, then pass that list to the view.
+
+	//In these two pages, you should display information based on the data passed
+	// from the controller.
+
+	@GetMapping ("/posts") // URL (e.g. http://localhost:8080/posts)
+	public String index(Model model) {
+		List<Post> posts = new ArrayList<>();
+		Post post = new Post("Title 1", "body 1");
+		Post post2 = new Post("Title 2", "body 2");
+
+		posts.add(post);
+		posts.add(post2);
+
+		model.addAttribute("posts", posts);
+
+		// path and file name
+		return "posts/index";
 	}
 
-	@GetMapping ("/posts/{id}")
-	public String show(@PathVariable long id) {
-		return "Will show the post with the id of: " + id;
+	@GetMapping ("/posts/{id}") // URL (e.g. http://localhost:8080/posts/2)
+	public String show(@PathVariable long id, Model model) {
+		Post post = new Post("Title goes here", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
+				"sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.");
+		model.addAttribute("post", post);
+
+		// path and file name
+		return "posts/show";
 	}
 
 	@GetMapping ("/posts/create")
@@ -25,6 +57,6 @@ public class PostController {
 
 	@PostMapping ("/posts/create")
 	public String save() {
-		return "Will display the created post";
+		return "Will create the post";
 	}
 }
--- src/main/java/com/codeup/blog/models/Post.java
+++ src/main/java/com/codeup/blog/models/Post.java
@@ -1,8 +1,22 @@
-package com.codeup.blog;
+package com.codeup.blog.models;
 
-public class POST {
+public class Post {
+	private Long id;
 	private String title;
 	private String body;
+
+	public Post(String title, String body) {
+		this.title = title;
+		this.body = body;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	public String getTitle() {
 		return title;
